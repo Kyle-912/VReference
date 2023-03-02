2c333116af2550ea284d630c7e9c3e2a973a6ea8
Arvain <35443465+Sukomi@users.noreply.github.com>
2023-03-01 21:49:52 -0500

    Added GET request for headsets json from backend

--- Frontend/my-app/src/app/app.component.ts
+++ Frontend/my-app/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  public headsetInfo: any;
+  public constructor(private http: HttpClient) { }
+  public ngOnInit(): void {
+    const url: string = 'http://localhost:4201/headsets';
+    this.http.get(url).subscribe((response) => {
+      this.headsetInfo = response;
+    });
+  }
 }
